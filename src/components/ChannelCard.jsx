d06c14b6c77d54fe3e436b91fdd0923f0a2fc7a5
lekan <oyeniranolalekan89@gmail.com>
2025-04-09 19:40:20 +0100

    fixed the deploy error

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Box, CardContent, CardMedia, Typography } from "@mui/material";
 import { CheckCircle } from "@mui/icons-material";
-import { Link } from "react-router-dom";
 import { demoProfilePicture } from "../utils/constants";
 
 const ChannelCard = ({ channelDetail, marginTop }) => {
